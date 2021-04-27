690c3d15b4a190ce57e8bede21af03bfa19fdc6c
Peter Zeltner <peter.zeltner@students.fhnw.ch>
2021-04-27 22:38:49 +0200

    fetch books from API

--- react-cqejgt/src/app/container/root-container/root-container.js
+++ react-cqejgt/src/app/container/root-container/root-container.js
@@ -1,15 +1,33 @@
 import React from 'react';
+import { useState, useEffect } from 'react'
 import { Example } from '../../components';
 
-class RootContainer extends React.PureComponent {
-  render() {
-    return (
-      <>
-        <h1>ti&m Library</h1>
-        <Example />
-      </>
-    );
+const RootContainer = () => {
+  const [books, setBooks] = useState([])
+
+  useEffect(() => {
+    const getBooks = async () => {
+      const rBooks = await fetchBooks()
+      setBooks(rBooks)
+    }
+    getBooks()
+  }, [])
+
+  const fetchBooks = async () => {
+    const res = await fetch('https://5c6eb0534fa1c9001424240b.mockapi.io/api/v1/books')
+    return await res.json()
   }
+
+  return (
+    <>
+      <h1>ti&m Library</h1>
+      <Example />
+      {books.map((book) => (
+        <p>{book.title}</p>
+      ))}
+    </>
+  );
+  
 }
 
 export default RootContainer;

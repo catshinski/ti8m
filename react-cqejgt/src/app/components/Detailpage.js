8074378c766882fb70c43eda121052e74aff95a2
Peter Zeltner <peter.zeltner@students.fhnw.ch>
2021-05-02 23:57:23 +0200

    finished iteration 2

--- react-cqejgt/src/app/components/Detailpage.js
+++ react-cqejgt/src/app/components/Detailpage.js
@@ -8,9 +8,12 @@ import Container from 'react-bootstrap/esm/Container'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Form from 'react-bootstrap/Form'
 
 function Detailpage(props) {
     const [detail, setDetail] = useState([])
+    const [tmpDetail, setTmpDetail] = useState([])
+    const [edit, setEdit] = useState(false)
     const {id} = useParams();
 
   useEffect(() => {
@@ -26,34 +29,82 @@ function Detailpage(props) {
     return await res.json()
   }
 
+  const toggleEditMode = () => {
+      setEdit(!edit)
+      setTmpDetail(detail)
+  }
+
+  const submitForm = async (e) => {
+      e.preventDefault()
+    const result = await fetch('https://5c6eb0534fa1c9001424240b.mockapi.io/api/v1/books/'+id, {
+        method: 'PUT',
+        headers : {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(tmpDetail),
+    })
+    setDetail(await result.json())
+    toggleEditMode()
+  }
+
     
     return (
         <Card>
             <Card.Header>
-                <Link to="/"><Button className="backButton">{'<'}</Button></Link>
+                <Link to="/"><Button className="headerButton">{'<'}</Button></Link>
                 <span className="detailHeader">Details</span>
+                <Button className="float-right headerButton" variant="outline-secondary" 
+                    onClick={toggleEditMode}>{edit ? 'Abbrechen' : 'Bearbeiten'}</Button>
             </Card.Header>
-            <Container>
-            <Row>
-                    <Col className="detailLabel" lg={2} xs={12}>Titel:</Col>
-                    <Col>{detail.title}</Col>
-                </Row>
-                <Row>
-                    <Col className="detailLabel" lg={2} xs={12}>Autor:</Col>
-                    <Col>{detail.author}</Col>
-                </Row>
-                <Row>
-                    <Col className="detailLabel" lg={2} xs={12}>Anzahl Exemplare:</Col>
-                    <Col>{detail.total_amount}</Col>
-                </Row>
-                <Row>
-                    <Col className="detailLabel" lg={2} xs={12}>Anzahl Seiten:</Col>
-                    <Col>{detail.pages}</Col>
-                </Row>
-                <Row>
-                    <Col className="detailLabel" lg={2} xs={12}>ISBN:</Col>
-                    <Col>{detail.isbn}</Col>
-                </Row>
+            <Container className="detailContent">
+                <Form onSubmit={submitForm}>
+                    <Row>
+                        <Col className="detailLabel" lg={2} xs={12}>Titel:</Col>
+                        <Col>
+                            { edit 
+                            ? <Form.Control type="text" value={tmpDetail.title} 
+                                onChange={(e) => setTmpDetail({...tmpDetail, title: e.target.value})}></Form.Control> 
+                            : detail.title }
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="detailLabel" lg={2} xs={12}>Autor:</Col>
+                        <Col>
+                            { edit 
+                            ? <Form.Control type="text" value={tmpDetail.author}
+                                onChange={(e) => setTmpDetail({...tmpDetail, author: e.target.value})}></Form.Control> 
+                            : detail.author }
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="detailLabel" lg={2} xs={12}>Anzahl Exemplare:</Col>
+                        <Col>
+                            { edit 
+                            ? <Form.Control type="number" value={tmpDetail.total_amount}
+                                onChange={(e) => setTmpDetail({...tmpDetail, total_amount: e.target.value})}></Form.Control> 
+                            : detail.total_amount }
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="detailLabel" lg={2} xs={12}>Anzahl Seiten:</Col>
+                        <Col>
+                            { edit 
+                            ? <Form.Control type="number" value={tmpDetail.pages}
+                                onChange={(e) => setTmpDetail({...tmpDetail, pages: e.target.value})}></Form.Control> 
+                            : detail.pages }
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="detailLabel" lg={2} xs={12}>ISBN:</Col>
+                        <Col>
+                            { edit 
+                            ? <Form.Control type="number" value={tmpDetail.isbn}
+                                onChange={(e) => setTmpDetail({...tmpDetail, isbn: e.target.value})}></Form.Control> 
+                            : detail.isbn }
+                        </Col>
+                    </Row>
+                    {edit && <Button type="submit" className="submitButton" >Speichern</Button>}
+                </Form>
             </Container>
         </Card>
     )
